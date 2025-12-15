e19650456c62a18aa57bd3a22fc04c37780fed57
PieroLan <hilter.lanche@upeu.edu.pe>
2025-12-14 22:01:14 -0500

    agregue la nueva configuracion de la bd postgres con docker

--- src/config/env/envs.ts
+++ src/config/env/envs.ts
@@ -4,8 +4,8 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   PORT_DB: get('PORT_DB').required().asPortNumber(),
-  USER: get('USER').required().asString(),
-  PASSWORD: get('PASSWORD').required().asString(),
-  DATABASE: get('DATABASE').required().asString(),
+  USER_DB: get('USER_DB').required().asString(),
+  PASSWORD_DB: get('PASSWORD_DB').required().asString(),
+  DATABASE_DB: get('DATABASE_DB').required().asString(),
   HOST: get('HOST').required().asString()
 };
